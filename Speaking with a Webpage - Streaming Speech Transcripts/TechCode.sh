944804f22200f71fb9469cda86f6e4a5b709a1cc
Prateek Rajput <144830423+prateekrajput08@users.noreply.github.com>
2025-09-07 20:09:58 +0530

    Update TechCode.sh

--- Speaking with a Webpage - Streaming Speech Transcripts/TechCode.sh
+++ Speaking with a Webpage - Streaming Speech Transcripts/TechCode.sh
@@ -1,25 +1,5 @@
 #!/bin/bash
 
-# Define color variables
-BLACK_TEXT=$'\033[0;90m'
-RED_TEXT=$'\033[0;91m'
-GREEN_TEXT=$'\033[0;92m'
-YELLOW_TEXT=$'\033[0;93m'
-BLUE_TEXT=$'\033[0;94m'
-MAGENTA_TEXT=$'\033[0;95m'
-CYAN_TEXT=$'\033[0;96m'
-WHITE_TEXT=$'\033[0;97m'
-
-NO_COLOR=$'\033[0m'
-RESET_FORMAT=$'\033[0m'
-
-# Define text formatting variables
-BOLD_TEXT=$'\033[1m'
-UNDERLINE_TEXT=$'\033[4m'
-
-clear
-
-
 # Welcome message
 echo "${BLUE_TEXT}${BOLD_TEXT}=======================================${RESET_FORMAT}"
 echo "${BLUE_TEXT}${BOLD_TEXT}         INITIATING EXECUTION...  ${RESET_FORMAT}"
