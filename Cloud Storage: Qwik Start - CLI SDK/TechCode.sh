7e36d15e31ae86900e480d0c3759b676f1f85793
Prateek Rajput <144830423+prateekrajput08@users.noreply.github.com>
2025-09-13 16:43:30 +0530

    Update TechCode.sh

--- Cloud Storage: Qwik Start - CLI SDK/TechCode.sh
+++ Cloud Storage: Qwik Start - CLI SDK/TechCode.sh
@@ -1,27 +1,28 @@
-
 #!/bin/bash
-# Enhanced Color Definitions
-COLOR_BLACK=$'\033[0;30m'
-COLOR_RED=$'\033[0;31m'
-COLOR_GREEN=$'\033[0;32m'
-COLOR_YELLOW=$'\033[0;33m'
-COLOR_BLUE=$'\033[0;34m'
-COLOR_MAGENTA=$'\033[0;35m'
-COLOR_CYAN=$'\033[0;36m'
-COLOR_WHITE=$'\033[0;37m'
-COLOR_RESET=$'\033[0m'
 
-# Text Formatting
-BOLD=$'\033[1m'
-UNDERLINE=$'\033[4m'
-BLINK=$'\033[5m'
-REVERSE=$'\033[7m'
+# Define color variables
+BLACK_TEXT=$'\033[0;90m'
+RED_TEXT=$'\033[0;91m'
+GREEN_TEXT=$'\033[0;92m'
+YELLOW_TEXT=$'\033[0;93m'
+BLUE_TEXT=$'\033[0;94m'
+MAGENTA_TEXT=$'\033[0;95m'
+CYAN_TEXT=$'\033[0;96m'
+WHITE_TEXT=$'\033[0;97m'
+
+NO_COLOR=$'\033[0m'
+RESET_FORMAT=$'\033[0m'
+
+# Define text formatting variables
+BOLD_TEXT=$'\033[1m'
+UNDERLINE_TEXT=$'\033[4m'
 
 clear
+
 # Welcome message
-echo "${COLOR_CYAN}${BOLD_TEXT}=======================================${RESET_FORMAT}"
-echo "${COLOR_CYAN}${BOLD_TEXT}         INITIATING EXECUTION...  ${RESET_FORMAT}"
-echo "${COLOR_CYAN}${BOLD_TEXT}=======================================${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}=======================================${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}         INITIATING EXECUTION...  ${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}=======================================${RESET_FORMAT}"
 echo
 
 
@@ -47,12 +48,11 @@ gsutil ls -l gs://$PROJECT_ID-techcode/ada.jpg
 
 gsutil acl ch -u AllUsers:R gs://$PROJECT_ID-techcode/ada.jpg
 
-
 # Final message
 echo
-echo "${COLOR_CYAN}${BOLD_TEXT}=======================================================${RESET_FORMAT}"
-echo "${COLOR_CYAN}${BOLD_TEXT}              LAB COMPLETED SUCCESSFULLY!              ${RESET_FORMAT}"
-echo "${COLOR_CYAN}${BOLD_TEXT}=======================================================${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}=======================================================${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}              LAB COMPLETED SUCCESSFULLY!              ${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}=======================================================${RESET_FORMAT}"
 echo
 echo "${RED_TEXT}${BOLD_TEXT}${UNDERLINE_TEXT}https://www.youtube.com/@TechCode9${RESET_FORMAT}"
 echo
