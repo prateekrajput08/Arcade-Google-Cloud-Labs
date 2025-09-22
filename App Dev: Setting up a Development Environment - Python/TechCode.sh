e24dc4b724b3801c32b18cbc02f323e4546b915b
Prateek Rajput <144830423+prateekrajput08@users.noreply.github.com>
2025-09-22 18:56:19 +0530

    Update TechCode.sh

--- App Dev: Setting up a Development Environment - Python/TechCode.sh
+++ App Dev: Setting up a Development Environment - Python/TechCode.sh
@@ -26,13 +26,24 @@ echo "${CYAN_TEXT}${BOLD_TEXT}=======================================${RESET_FOR
 echo
 
 
-gcloud compute instances create dev-instance --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --machine-type=e2-medium --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default --metadata=enable-oslogin=true --maintenance-policy=MIGRATE --provisioning-model=STANDARD --scopes=https://www.googleapis.com/auth/cloud-platform --tags=http-server --create-disk=auto-delete=yes,boot=yes,device-name=dev-instance,image=projects/debian-cloud/global/images/debian-11-bullseye-v20240213,mode=rw,size=10,type=projects/$DEVSHELL_PROJECT_ID/zones/$ZONE/diskTypes/pd-balanced --no-shielded-secure-boot --shielded-vtpm --shielded-integrity-monitoring --labels=goog-ec-src=vm_add-gcloud --reservation-affinity=any
+gcloud auth list
 
-gcloud compute firewall-rules create allow-http --allow tcp:80 --description "Allow HTTP traffic" --direction INGRESS --target-tags http-server --network default
+gcloud config set compute/zone $ZONE 
 
-sleep 30
+export REGION=${ZONE%-*}
+gcloud config set compute/region $REGION
 
-cat > prepare_disk.sh <<'EOF_END'
+export PROJECT_ID=$(gcloud config get-value project)
+
+export PROJECT_ID=$DEVSHELL_PROJECT_ID
+
+gcloud compute instances create dev-instance --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --machine-type=e2-standard-2 --scopes=https://www.googleapis.com/auth/cloud-platform --tags=http-server --create-disk=auto-delete=yes,boot=yes,image=projects/debian-cloud/global/images/family/debian-11,mode=rw,size=10,type=projects/$DEVSHELL_PROJECT_ID/zones/$ZONE/diskTypes/pd-balanced --no-shielded-secure-boot --shielded-vtpm --shielded-integrity-monitoring --labels=goog-ec-src=vm_add-gcloud --reservation-affinity=any
+
+gcloud compute firewall-rules create allow-http --action=ALLOW --direction=INGRESS --description "Allow HTTP traffic" --rules=tcp:80 --source-ranges=0.0.0.0/0 --target-tags=http-server
+
+sleep 10
+
+cat > TechCode.sh <<'EOF_CP'
 sudo apt-get update
 sudo apt-get install git -y
 sudo apt-get install python3-setuptools python3-dev build-essential -y
@@ -44,13 +55,13 @@ git clone https://github.com/GoogleCloudPlatform/training-data-analyst
 ln -s ~/training-data-analyst/courses/developingapps/v1.3/python/devenv ~/devenv
 cd ~/devenv/
 sudo python3 server.py
-EOF_END
+EOF_CP
 
-gcloud compute scp prepare_disk.sh dev-instance:/tmp --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --quiet
+sleep 10
 
-gcloud compute ssh dev-instance --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --quiet --command="bash /tmp/prepare_disk.sh"
+gcloud compute scp TechCode.sh dev-instance:/tmp --project="$DEVSHELL_PROJECT_ID" --zone="$ZONE" --quiet
 
-
+gcloud compute ssh dev-instance --project="$DEVSHELL_PROJECT_ID" --zone="$ZONE" --quiet --command='bash /tmp/TechCode.sh'
 # Final message
 echo
 echo "${CYAN_TEXT}${BOLD_TEXT}=======================================================${RESET_FORMAT}"
