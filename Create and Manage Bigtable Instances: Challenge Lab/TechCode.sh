42e1e6cb011e3bd29a18cdb3faef51201600d71c
Prateek Rajput <144830423+prateekrajput08@users.noreply.github.com>
2025-11-02 22:04:17 +0530

    Update TechCode.sh

--- Create and Manage Bigtable Instances: Challenge Lab/TechCode.sh
+++ Create and Manage Bigtable Instances: Challenge Lab/TechCode.sh
@@ -80,10 +80,10 @@ while true; do
     gcloud dataflow jobs run import-sessions --region=$REGION --project=$PROJECT_ID --gcs-location gs://dataflow-templates-$REGION/latest/GCS_SequenceFile_to_Cloud_Bigtable --staging-location gs://$PROJECT_ID/temp --parameters bigtableProject=$PROJECT_ID,bigtableInstanceId=ecommerce-recommendations,bigtableTableId=SessionHistory,sourcePattern=gs://cloud-training/OCBL377/retail-engagements-sales-00000-of-00001,mutationThrottleLatencyMs=0
 
     if [ $? -eq 0 ]; then
-        echo -e "\033[1;33mJob has completed successfully. now just wait for succeeded\033[0m \033[1;34mhttps://www.youtube.com/@chayandeokar\033[0m"
+        echo -e "\033[1;33mJob has completed successfully. now just wait for succeeded\033[0m \033[1;34m\033[0m"
         break
     else
-        echo -e "\033[1;33mJob retrying. please like share and subscribe to techcps\033[0m \033[1;34mhttps://www.youtube.com/@chayandeokar\033[0m"
+        echo -e "\033[1;33mJob retrying. please like share and subscribe to techcps\033[0m \033[1;34m\033[0m"
         sleep 10
     fi
 done
@@ -99,10 +99,10 @@ while true; do
     gcloud dataflow jobs run import-recommendations --region=$REGION --project=$PROJECT_ID --gcs-location gs://dataflow-templates-$REGION/latest/GCS_SequenceFile_to_Cloud_Bigtable --staging-location gs://$PROJECT_ID/temp --parameters bigtableProject=$PROJECT_ID,bigtableInstanceId=ecommerce-recommendations,bigtableTableId=PersonalizedProducts,sourcePattern=gs://cloud-training/OCBL377/retail-recommendations-00000-of-00001,mutationThrottleLatencyMs=0
 
     if [ $? -eq 0 ]; then
-        echo -e "\033[1;33mJob has completed successfully. now just wait for succeeded\033[0m \033[1;34mhttps://www.youtube.com/@chayandeokar\033[0m"
+        echo -e "\033[1;33mJob has completed successfully. now just wait for succeeded\033[0m \033[1;34m\033[0m"
         break
     else
-        echo -e "\033[1;33mJob retrying. please like share and subscribe to techcps\033[0m \033[1;34mhttps://www.youtube.com/@chayandeokar\033[0m"
+        echo -e "\033[1;33mJob retrying. please like share and subscribe to techcps\033[0m \033[1;34m\033[0m"
         sleep 10
     fi
 done
