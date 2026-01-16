8dfbad5f24b258e390c7bc047277869469c4951f
Prateek Rajput <144830423+prateekrajput08@users.noreply.github.com>
2026-01-16 13:36:53 +0530

    Update TechCode.sh

--- Creating and Alerting on Logs-based Metrics/TechCode.sh
+++ Creating and Alerting on Logs-based Metrics/TechCode.sh
@@ -17,35 +17,6 @@ RESET_FORMAT=$'\033[0m'
 BOLD_TEXT=$'\033[1m'
 UNDERLINE_TEXT=$'\033[4m'
 
-
-
-BLACK=`tput setaf 0`
-RED=`tput setaf 1`
-GREEN=`tput setaf 2`
-YELLOW=`tput setaf 3`
-BLUE=`tput setaf 4`
-MAGENTA=`tput setaf 5`
-CYAN=`tput setaf 6`
-WHITE=`tput setaf 7`
-
-BG_BLACK=`tput setab 0`
-BG_RED=`tput setab 1`
-BG_GREEN=`tput setab 2`
-BG_YELLOW=`tput setab 3`
-BG_BLUE=`tput setab 4`
-BG_MAGENTA=`tput setab 5`
-BG_CYAN=`tput setab 6`
-BG_WHITE=`tput setab 7`
-
-
-# Array of color codes excluding black and white
-TEXT_COLORS=($RED $GREEN $YELLOW $BLUE $MAGENTA $CYAN)
-BG_COLORS=($BG_RED $BG_GREEN $BG_YELLOW $BG_BLUE $BG_MAGENTA $BG_CYAN)
-
-# Pick random colors
-RANDOM_TEXT_COLOR=${TEXT_COLORS[$RANDOM % ${#TEXT_COLORS[@]}]}
-RANDOM_BG_COLOR=${BG_COLORS[$RANDOM % ${#BG_COLORS[@]}]}
-
 clear
 
 # Welcome message
@@ -54,10 +25,8 @@ echo "${CYAN_TEXT}${BOLD_TEXT}      SUBSCRIBE TECH & CODE- INITIATING EXECUTION.
 echo "${CYAN_TEXT}${BOLD_TEXT}==================================================================${RESET_FORMAT}"
 echo
 
-
-
 # Step 1: Set Project ID, Compute Zone & Region
-echo "${BOLD}${GREEN}Setting Project ID, Compute Zone & Region${RESET}"
+echo "${BOLD_TEXT}${YELLOW_TEXT}Setting Project ID, Compute Zone & Region${RESET_FORMAT}"
 export PROJECT_ID=$(gcloud info --format='value(config.project)')
 
 export ZONE=$(gcloud compute project-info describe \
@@ -69,17 +38,17 @@ export REGION=$(gcloud compute project-info describe \
 gcloud config set compute/zone $ZONE
 
 # Step 2: Create Kubernetes Cluster
-echo "${BOLD}${CYAN}Creating Kubernetes Cluster${RESET}"
+echo "${BOLD_TEXT}${YELLOW_TEXT}Creating Kubernetes Cluster${RESET_FORMAT}"
 gcloud container clusters create gmp-cluster --num-nodes=1 --zone $ZONE
 
 # Step 3: Create Logging Metric for Stopped VMs
-echo "${BOLD}${RED}Creating log-based metric for stopped VMs${RESET}"
+echo "${BOLD_TEXT}${YELLOW_TEXT}Creating log-based metric for stopped VMs${RESET_FORMAT}"
 gcloud logging metrics create stopped-vm \
     --description="Metric for stopped VMs" \
     --log-filter='resource.type="gce_instance" protoPayload.methodName="v1.compute.instances.stop"'
 
 # Step 4: Create Pub/Sub notification channel config file
-echo "${BOLD}${GREEN}Creating Pub/Sub notification channel config file${RESET}"
+echo "${BOLD_TEXT}${YELLOW_TEXT}Creating Pub/Sub notification channel config file${RESET_FORMAT}"
 cat > pubsub-channel.json <<EOF_END
 {
   "type": "pubsub",
@@ -92,16 +61,16 @@ cat > pubsub-channel.json <<EOF_END
 EOF_END
 
 # Step 5: Create the Pub/Sub notification channel
-echo "${BOLD}${YELLOW}Creating Pub/Sub notification channel${RESET}"
+echo "${BOLD_TEXT}${YELLOW_TEXT}Creating Pub/Sub notification channel${RESET_FORMAT}"
 gcloud beta monitoring channels create --channel-content-from-file=pubsub-channel.json
 
 # Step 6: Retrieve Notification Channel ID
-echo "${BOLD}${BLUE}Retrieving Notification Channel ID${RESET}"
+echo "${BOLD}${YELLOW_TEXT}Retrieving Notification Channel ID${RESET_FORMAT}"
 email_channel_info=$(gcloud beta monitoring channels list)
 email_channel_id=$(echo "$email_channel_info" | grep -oP 'name: \K[^ ]+' | head -n 1)
 
 # Step 7: Create Alert Policy for Stopped VMs
-echo "${BOLD}${MAGENTA}Creating alert policy for stopped VMs${RESET}"
+echo "${BOLD_TEXT}${YELLOW_TEXT}Creating alert policy for stopped VMs${RESET_FORMAT}"
 cat > stopped-vm-alert-policy.json <<EOF_END
 {
   "displayName": "stopped vm",
@@ -135,23 +104,23 @@ cat > stopped-vm-alert-policy.json <<EOF_END
 EOF_END
 
 # Step 8: Deploy Alert Policy
-echo "${BOLD}${CYAN}Deploying alert policy for stopped VMs${RESET}"
+echo "${BOLD_TEXT}${YELLOW_TEXT}Deploying alert policy for stopped VMs${RESET_FORMAT}"
 gcloud alpha monitoring policies create --policy-from-file=stopped-vm-alert-policy.json
 
 # Step 9: Create Artifact Registry
-echo "${BOLD}${RED}Creating Docker Artifact Registry${RESET}"
+echo "${BOLD_TEXT}${YELLOW_TEXT}Creating Docker Artifact Registry${RESET_FORMAT}"
 gcloud artifacts repositories create docker-repo --repository-format=docker \
     --location=$REGION --description="Docker repository" \
     --project=$DEVSHELL_PROJECT_ID
 
 # Step 10: Download and Load Docker Image
-echo "${BOLD}${GREEN}Downloading and loading Docker image${RESET}"
+echo "${BOLD_TEXT}${YELLOW_TEXT}Downloading and loading Docker image${RESET_FORMAT}"
  wget https://storage.googleapis.com/spls/gsp1024/flask_telemetry.zip
  unzip flask_telemetry.zip
  docker load -i flask_telemetry.tar
 
 # Step 11: Tag and Push Docker Image
-echo "${BOLD}${YELLOW}Tagging and pushing Docker image${RESET}"
+echo "${BOLD_TEXT}${YELLOW_TEXT}Tagging and pushing Docker image${RESET_FORMAT}"
 docker tag gcr.io/ops-demo-330920/flask_telemetry:61a2a7aabc7077ef474eb24f4b69faeab47deed9 \
 $REGION-docker.pkg.dev/$DEVSHELL_PROJECT_ID/docker-repo/flask-telemetry:v1
 
@@ -160,35 +129,35 @@ docker push $REGION-docker.pkg.dev/$DEVSHELL_PROJECT_ID/docker-repo/flask-teleme
 gcloud container clusters list
 
 # Step 12: Get Cluster Credentials
-echo "${BOLD}${BLUE}Getting Kubernetes cluster credentials${RESET}"
+echo "${BOLD_TEXT}${YELLOW_TEXT}Getting Kubernetes cluster credentials${RESET_FORMAT}"
 gcloud container clusters get-credentials gmp-cluster
 
 # Step 13: Create Namespace
-echo "${BOLD}${MAGENTA}Creating Kubernetes namespace${RESET}"
+echo "${BOLD_TEXT}${YELLOW_TEXT}Creating Kubernetes namespace${RESET_FORMAT}"
 kubectl create ns gmp-test
 
 # Step 14: Download and Unpack Prometheus Setup
-echo "${BOLD}${CYAN}Downloading and unpacking Prometheus setup files${RESET}"
+echo "${BOLD_TEXT}${YELLOW_TEXT}Downloading and unpacking Prometheus setup files${RESET_FORMAT}"
 wget https://storage.googleapis.com/spls/gsp1024/gmp_prom_setup.zip
 unzip gmp_prom_setup.zip
 cd gmp_prom_setup
 
 # Step 15: Update Deployment with Docker Image
-echo "${BOLD}${RED}Updating deployment manifest with Docker image URL${RESET}"
+echo "${BOLD_TEXT}${YELLOW_TEXT}Updating deployment manifest with Docker image URL${RESET_FORMAT}"
 sed -i "s|<ARTIFACT REGISTRY IMAGE NAME>|$REGION-docker.pkg.dev/$DEVSHELL_PROJECT_ID/docker-repo/flask-telemetry:v1|g" flask_deployment.yaml
 
 # Step 16: Apply Kubernetes Resources
-echo "${BOLD}${GREEN}Applying Kubernetes deployment and service${RESET}"
+echo "${BOLD_TEXT}${YELLOW_TEXT}Applying Kubernetes deployment and service${RESET_FORMAT}"
 kubectl -n gmp-test apply -f flask_deployment.yaml
 
 kubectl -n gmp-test apply -f flask_service.yaml
 
 # Step 17: Check Services
-echo "${BOLD}${YELLOW}Checking Kubernetes services${RESET}"
+echo "${BOLD_TEXT}${YELLOW_TEXT}Checking Kubernetes services${RESET_FORMAT}"
 kubectl get services -n gmp-test
 
 # Step 18: Create Metric for hello-app Errors
-echo "${BOLD}${BLUE}Creating log-based metric for hello-app errors${RESET}"
+echo "${BOLD_TEXT}${YELLOW_TEXT}Creating log-based metric for hello-app errors${RESET_FORMAT}"
 gcloud logging metrics create hello-app-error \
     --description="Metric for hello-app errors" \
     --log-filter='severity=ERROR
@@ -198,7 +167,7 @@ textPayload: "ERROR: 404 Error page not found"'
 sleep 30
 
 # Step 19: Create Alert Policy for hello-app Errors
-echo "${BOLD}${MAGENTA}Creating alert policy for hello-app errors${RESET}"
+echo "${BOLD_TEXT}${YELLOW_TEXT}Creating alert policy for hello-app errors${RESET_FORMAT}"
 cat > awesome.json <<'EOF_END'
 {
   "displayName": "log based metric alert",
@@ -235,17 +204,18 @@ cat > awesome.json <<'EOF_END'
 EOF_END
 
 # Step 20: Deploy Alert Policy
-echo "${BOLD}${CYAN}Deploying alert policy for hello-app errors${RESET}"
+echo "${BOLD_TEXT}${YELLOW_TEXT}Deploying alert policy for hello-app errors${RESET_FORMAT}"
 gcloud alpha monitoring policies create --policy-from-file=awesome.json
 
 # Step 21: Trigger Errors
-echo "${BOLD}${RED}Triggering errors to generate logs for metric${RESET}"
+echo "${BOLD_TEXT}${YELLOW_TEXT}Triggering errors to generate logs for metric${RESET_FORMAT}"
 timeout 120 bash -c -- 'while true; do curl $(kubectl get services -n gmp-test -o jsonpath='{.items[*].status.loadBalancer.ingress[0].ip}')/error; sleep $((RANDOM % 4)) ; done'
 
 echo
 
-
-cd
+# Display completion message
+echo "${BOLD_TEXT}${YELLOW_TEXT}Lab execution completed successfully!${RESET_FORMAT}"
+echo "${BOLD_TEXT}${YELLOW_TEXT}Thank you for using Dr. Abhishek Cloud Tutorials${RESET_FORMAT}"
 
 remove_files() {
     # Loop through all files in the current directory
@@ -263,7 +233,6 @@ remove_files() {
 }
 
 remove_files
-
 
 # Final message
 echo
