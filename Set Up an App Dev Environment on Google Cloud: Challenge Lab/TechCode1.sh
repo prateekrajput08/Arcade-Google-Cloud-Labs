4df9a25270bdb118e09efca64869656e75dea3ff
Prateek Rajput <144830423+prateekrajput08@users.noreply.github.com>
2026-03-15 20:01:23 +0530

    Update and rename TechCode.sh to TechCode1.sh

---
+++ Set Up an App Dev Environment on Google Cloud: Challenge Lab/TechCode1.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+BLACK_TEXT=$'\033[0;90m'
+RED_TEXT=$'\033[0;91m'
+GREEN_TEXT=$'\033[0;92m'
+YELLOW_TEXT=$'\033[0;93m'
+BLUE_TEXT=$'\033[0;94m'
+MAGENTA_TEXT=$'\033[0;95m'
+CYAN_TEXT=$'\033[0;96m'
+WHITE_TEXT=$'\033[0;97m'
+
+BOLD_TEXT=$'\033[1m'
+UNDERLINE_TEXT=$'\033[4m'
+RESET_FORMAT=$'\033[0m'
+
+clear
+
+echo "${CYAN_TEXT}${BOLD_TEXT}==================================================================${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}      SUBSCRIBE TECH & CODE- INITIATING EXECUTION...  ${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}==================================================================${RESET_FORMAT}"
+echo
+
+echo "${YELLOW_TEXT}${BOLD_TEXT}Please enter required values:${RESET_FORMAT}"
+echo
+
+read -p "Enter User Email to Remove: " USER_2
+read -p "Enter Zone (example europe-west4-a): " ZONE
+read -p "Enter Pub/Sub Topic Name: " TOPIC
+read -p "Enter Cloud Function Name: " FUNCTION
+
+export REGION="${ZONE%-*}"
+
+# ===============================
+# ENABLE SERVICES
+# ===============================
+
+gcloud services enable \
+artifactregistry.googleapis.com \
+cloudfunctions.googleapis.com \
+cloudbuild.googleapis.com \
+eventarc.googleapis.com \
+run.googleapis.com \
+logging.googleapis.com \
+pubsub.googleapis.com
+
+sleep 90
+
+PROJECT_NUMBER=$(gcloud projects describe $DEVSHELL_PROJECT_ID --format='value(projectNumber)')
+
+gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID \
+--member=serviceAccount:$PROJECT_NUMBER-compute@developer.gserviceaccount.com \
+--role=roles/eventarc.eventReceiver
+
+sleep 30
+
+SERVICE_ACCOUNT="$(gsutil kms serviceaccount -p $DEVSHELL_PROJECT_ID)"
+
+gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID \
+--member="serviceAccount:${SERVICE_ACCOUNT}" \
+--role='roles/pubsub.publisher'
+
+sleep 30
+
+gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID \
+--member="serviceAccount:service-$PROJECT_NUMBER@gcp-sa-eventarc.iam.gserviceaccount.com" \
+--role="roles/storage.admin"
+
+sleep 30
+
+# Eventarc bucket permission fix
+gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID \
+--member="serviceAccount:service-$PROJECT_NUMBER@gcp-sa-eventarc.iam.gserviceaccount.com" \
+--role="roles/storage.admin"
+
+sleep 30
+
+echo
+echo "${CYAN_TEXT}${BOLD_TEXT}=======================================================${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}              LAB COMPLETED SUCCESSFULLY!              ${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}=======================================================${RESET_FORMAT}"
