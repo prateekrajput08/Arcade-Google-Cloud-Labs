c89100d7ea3971005bf29d87f5411417d6fb40da
Prateek Rajput <144830423+prateekrajput08@users.noreply.github.com>
2026-03-15 20:04:25 +0530

    Create TechCode2.sh

---
+++ Set Up an App Dev Environment on Google Cloud: Challenge Lab/TechCode2.sh
@@ -0,0 +1,138 @@
+#!/bin/bash
+
+REGION="europe-west4"
+FUNCTION="memories-thumbnail-generator"
+
+echo "Getting project info..."
+
+PROJECT_ID=$(gcloud config get-value project)
+
+BUCKET=$(gsutil ls | head -n1 | sed 's/gs:\/\///' | sed 's/\///')
+
+TOPIC=$(gcloud pubsub topics list --format="value(name)" | head -n1 | awk -F'/' '{print $NF}')
+
+echo "Project: $PROJECT_ID"
+echo "Bucket: $BUCKET"
+echo "Topic: $TOPIC"
+
+echo "Enabling APIs..."
+
+gcloud services enable \
+cloudfunctions.googleapis.com \
+run.googleapis.com \
+eventarc.googleapis.com \
+pubsub.googleapis.com \
+cloudbuild.googleapis.com
+
+sleep 60
+
+mkdir task3
+cd task3
+
+cat > index.js <<EOF
+const functions = require('@google-cloud/functions-framework');
+const { Storage } = require('@google-cloud/storage');
+const { PubSub } = require('@google-cloud/pubsub');
+const sharp = require('sharp');
+
+functions.cloudEvent('$FUNCTION', async cloudEvent => {
+
+  const event = cloudEvent.data;
+
+  const fileName = event.name;
+  const bucketName = event.bucket;
+
+  const bucket = new Storage().bucket(bucketName);
+
+  const topicName = "$TOPIC";
+  const pubsub = new PubSub();
+
+  if (fileName.search("64x64_thumbnail") === -1) {
+
+    const filename_split = fileName.split('.');
+    const filename_ext = filename_split[filename_split.length - 1].toLowerCase();
+    const filename_without_ext = fileName.substring(0, fileName.length - filename_ext.length - 1);
+
+    if (filename_ext === 'png' || filename_ext === 'jpg' || filename_ext === 'jpeg') {
+
+      const gcsObject = bucket.file(fileName);
+      const newFilename = \`\${filename_without_ext}_64x64_thumbnail.\${filename_ext}\`;
+      const gcsNewObject = bucket.file(newFilename);
+
+      try {
+
+        const [buffer] = await gcsObject.download();
+
+        const resizedBuffer = await sharp(buffer)
+          .resize(64,64,{fit:'inside',withoutEnlargement:true})
+          .toFormat(filename_ext)
+          .toBuffer();
+
+        await gcsNewObject.save(resizedBuffer);
+
+        await pubsub.topic(topicName)
+          .publishMessage({data:Buffer.from(newFilename)});
+
+      } catch(err){
+        console.error(err);
+      }
+
+    }
+  }
+});
+EOF
+
+
+cat > package.json <<EOF
+{
+"name":"thumbnails",
+"version":"1.0.0",
+"description":"Create Thumbnail",
+"scripts":{"start":"node index.js"},
+"dependencies":{
+"@google-cloud/functions-framework":"^3.0.0",
+"@google-cloud/pubsub":"^2.0.0",
+"@google-cloud/storage":"^6.11.0",
+"sharp":"^0.32.1"
+}
+}
+EOF
+
+
+echo "Deploying Cloud Run Function..."
+
+gcloud functions deploy $FUNCTION \
+--gen2 \
+--runtime=nodejs22 \
+--region=$REGION \
+--entry-point=$FUNCTION \
+--trigger-resource=$BUCKET \
+--trigger-event=google.storage.object.finalize \
+--source=.
+
+
+echo "Testing function..."
+
+curl -O https://storage.googleapis.com/cloud-training/gsp315/map.jpg
+
+gsutil cp map.jpg gs://$BUCKET
+
+echo "Task 3 Completed."
+
+echo "Removing previous cloud engineer..."
+
+VIEWER_USER=$(gcloud projects get-iam-policy $(gcloud config get-value project) \
+--flatten="bindings[].members" \
+--filter="bindings.role:roles/viewer" \
+--format="value(bindings.members)")
+
+gcloud projects remove-iam-policy-binding $(gcloud config get-value project) \
+--member="$VIEWER_USER" \
+--role="roles/viewer"
+
+echo "Previous engineer removed."
+
+echo
+echo "${CYAN_TEXT}${BOLD_TEXT}=======================================================${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}              LAB COMPLETED SUCCESSFULLY!              ${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}=======================================================${RESET_FORMAT}"
