a4882162d48ae95dd0362e2ec704309a089db3e5
Prateek Rajput <144830423+prateekrajput08@users.noreply.github.com>
2026-04-02 13:58:05 +0530

    Create TechCode.sh

---
+++ Exploring Cost-optimization for GKE Virtual Machines/TechCode.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+
+# Define color variables
+BLACK_TEXT=$'\033[0;90m'
+RED_TEXT=$'\033[0;91m'
+GREEN_TEXT=$'\033[0;92m'
+YELLOW_TEXT=$'\033[0;93m'
+BLUE_TEXT=$'\033[0;94m'
+MAGENTA_TEXT=$'\033[0;95m'
+CYAN_TEXT=$'\033[0;96m'
+WHITE_TEXT=$'\033[0;97m'
+TEAL=$'\033[38;5;50m'
+
+# Define text formatting variables
+BOLD_TEXT=$'\033[1m'
+UNDERLINE_TEXT=$'\033[4m'
+BLINK_TEXT=$'\033[5m'
+NO_COLOR=$'\033[0m'
+RESET_FORMAT=$'\033[0m'
+REVERSE_TEXT=$'\033[7m'
+
+clear
+
+# Welcome message
+echo "${CYAN_TEXT}${BOLD_TEXT}==================================================================${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}      SUBSCRIBE TECH & CODE- INITIATING EXECUTION...  ${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}==================================================================${RESET_FORMAT}"
+echo
+
+echo "${YELLOW_TEXT}${BOLD_TEXT}Fetching Zone and Region...${RESET_FORMAT}"
+
+ZONE=$(gcloud compute project-info describe \
+  --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
+REGION=$(gcloud compute project-info describe \
+  --format="value(commonInstanceMetadata.items[google-compute-default-region])")
+PROJECT_ID=$(gcloud config get-value project)
+
+
+echo "${YELLOW_TEXT}${BOLD_TEXT}Creating Cluster...${RESET_FORMAT}"
+
+gcloud container clusters get-credentials hello-demo-cluster --zone $ZONE
+
+kubectl scale deployment hello-server --replicas=2
+
+gcloud container clusters resize hello-demo-cluster --node-pool my-node-pool \
+    --num-nodes 3 --zone $ZONE --quiet
+
+
+gcloud container node-pools create larger-pool \
+  --cluster=hello-demo-cluster \
+  --machine-type=e2-standard-2 \
+  --num-nodes=1 \
+  --zone=$ZONE
+
+
+for node in $(kubectl get nodes -l cloud.google.com/gke-nodepool=my-node-pool -o=name); do
+  kubectl cordon "$node";
+done
+
+
+for node in $(kubectl get nodes -l cloud.google.com/gke-nodepool=my-node-pool -o=name); do
+  kubectl drain --force --ignore-daemonsets --delete-emptydir-data --grace-period=10 "$node";
+done
+
+
+gcloud container node-pools delete my-node-pool --cluster hello-demo-cluster --zone $ZONE --quiet 
+
+gcloud container clusters create regional-demo --region=$REGION --num-nodes=1
+
+cat << EOF > pod-1.yaml
+apiVersion: v1
+kind: Pod
+metadata:
+  name: pod-1
+  labels:
+    security: demo
+spec:
+  containers:
+  - name: container-1
+    image: wbitt/network-multitool
+EOF
+
+kubectl apply -f pod-1.yaml
+
+cat << EOF > pod-2.yaml
+apiVersion: v1
+kind: Pod
+metadata:
+  name: pod-2
+spec:
+  affinity:
+    podAntiAffinity:
+      requiredDuringSchedulingIgnoredDuringExecution:
+      - labelSelector:
+          matchExpressions:
+          - key: security
+            operator: In
+            values:
+            - demo
+        topologyKey: "kubernetes.io/hostname"
+  containers:
+  - name: container-2
+    image: us-docker.pkg.dev/google-samples/containers/gke/hello-app:1.0
+EOF
+
+kubectl apply -f pod-2.yaml
+
+
+# Final message
+echo
+echo "${CYAN_TEXT}${BOLD_TEXT}=======================================================${RESET_FORMAT_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}             Follow Video For Other Tasks              ${RESET_FORMAT_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}=======================================================${RESET_FORMAT_FORMAT}"
+echo
+echo "${RED_TEXT}${BOLD_TEXT}${UNDERLINE_TEXT}https://www.youtube.com/@TechCode9${RESET_FORMAT_FORMAT}"
+echo "
