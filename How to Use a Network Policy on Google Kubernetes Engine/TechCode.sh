9feac2a6bdabec564cb1cfb5b7e45b9496956b35
Prateek Rajput <144830423+prateekrajput08@users.noreply.github.com>
2026-05-23 20:24:38 +0530

    Update TechCode.sh

--- How to Use a Network Policy on Google Kubernetes Engine/TechCode.sh
+++ How to Use a Network Policy on Google Kubernetes Engine/TechCode.sh
@@ -64,7 +64,9 @@ echo
 
 printf "y\n" | make setup-project
 
-sleep 15
+echo
+echo "${YELLOW_TEXT}Waiting for APIs to finish enabling...${RESET_FORMAT}"
+sleep 180
 
 echo
 echo "${GREEN_TEXT}Project setup completed.${RESET_FORMAT}"
@@ -83,14 +85,68 @@ terraform apply -auto-approve
 cd ..
 
 echo
+echo "${GREEN_TEXT}${BOLD_TEXT}Copying Lab Files to Bastion VM...${RESET_FORMAT}"
+echo
+
+gcloud compute scp --recurse ~/gke-network-policy-demo \
+gke-demo-bastion:~ --zone "$ZONE"
+
+echo
+echo "${GREEN_TEXT}${BOLD_TEXT}Connecting to Bastion VM...${RESET_FORMAT}"
+echo
+
+gcloud compute ssh gke-demo-bastion --zone "$ZONE" << EOF
+
+export ZONE=\$(gcloud config get-value compute/zone)
+
+cd ~/gke-network-policy-demo || exit 1
+
+sudo apt-get update
+
+sudo apt-get install -y google-cloud-sdk-gke-gcloud-auth-plugin
+
+echo "export USE_GKE_GCLOUD_AUTH_PLUGIN=True" >> ~/.bashrc
+
+source ~/.bashrc
+
+gcloud container clusters get-credentials gke-demo-cluster --zone "\$ZONE"
+
+kubectl apply -f ./manifests/hello-app/
+
+kubectl get pods
+
+timeout 10 kubectl logs --tail 10 -f \$(kubectl get pods -oname -l app=hello)
+
+timeout 10 kubectl logs --tail 10 -f \$(kubectl get pods -oname -l app=not-hello)
+
+kubectl apply -f ./manifests/network-policy.yaml
+
+timeout 10 kubectl logs --tail 10 -f \$(kubectl get pods -oname -l app=not-hello)
+
+kubectl delete -f ./manifests/network-policy.yaml
+
+kubectl create -f ./manifests/network-policy-namespaced.yaml
+
+timeout 10 kubectl logs --tail 10 -f \$(kubectl get pods -oname -l app=hello)
+
+kubectl -n hello-apps apply -f ./manifests/hello-app/hello-client.yaml
+
+timeout 10 kubectl logs --tail 10 -f -n hello-apps \
+\$(kubectl get pods -oname -l app=hello -n hello-apps)
+
+exit
+EOF
+
+echo
 echo "${CYAN_TEXT}${BOLD_TEXT}=======================================================${RESET_FORMAT}"
 echo "${CYAN_TEXT}${BOLD_TEXT}              LAB COMPLETED SUCCESSFULLY!              ${RESET_FORMAT}"
 echo "${CYAN_TEXT}${BOLD_TEXT}=======================================================${RESET_FORMAT}"
 echo
 
 echo "${RED_TEXT}${BOLD_TEXT}${UNDERLINE_TEXT}https://www.youtube.com/@TechCode9${RESET_FORMAT}"
-
-echo
 echo "${GREEN_TEXT}${BOLD_TEXT}Don't forget to Like, Share & Subscribe${RESET_FORMAT}"
 echo
+
+echo "${YELLOW_TEXT}${BOLD_TEXT}Cleaning up temporary script...${RESET_FORMAT}"
+
 rm -f TechCode.sh
