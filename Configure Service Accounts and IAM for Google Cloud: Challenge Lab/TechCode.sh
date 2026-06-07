2f8415fe44b143f7e6e92b86216f304740c1e94a
Prateek Rajput <144830423+prateekrajput08@users.noreply.github.com>
2026-06-07 19:11:50 +0530

    Update TechCode.sh

--- Configure Service Accounts and IAM for Google Cloud: Challenge Lab/TechCode.sh
+++ Configure Service Accounts and IAM for Google Cloud: Challenge Lab/TechCode.sh
@@ -12,8 +12,6 @@ WHITE_TEXT=$'\033[0;97m'
 
 NO_COLOR=$'\033[0m'
 RESET_FORMAT=$'\033[0m'
-
-# Define text formatting variables
 BOLD_TEXT=$'\033[1m'
 UNDERLINE_TEXT=$'\033[4m'
 
@@ -50,7 +48,11 @@ gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID --member serviceAcco
 
 gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID --member serviceAccount:$SA --role=roles/compute.instanceAdmin
 
-gcloud compute instances create vm-2 --machine-type e2-micro --service-account $SA --zone $ZONE --scopes "https://www.googleapis.com/auth/compute"
+gcloud compute instances create vm-2 \
+--machine-type=e2-micro \
+--service-account=$SA \
+--zone=$ZONE \
+--scopes=https://www.googleapis.com/auth/compute
 
 cat > role-definition.yaml <<EOF
 title: "My Company Admin"
@@ -72,7 +74,11 @@ gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID --member=serviceAcco
 
 gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID --member=serviceAccount:$SA --role=roles/bigquery.user
 
-gcloud compute instances create bigquery-instance --service-account=$SA --scopes=https://www.googleapis.com/auth/bigquery --zone=$ZONE
+gcloud compute instances create bigquery-instance \
+--service-account=$SA \
+--scopes=https://www.googleapis.com/auth/cloud-platform \
+--zone=$ZONE
+
 EOF_END
 
 gcloud compute scp prepare_disk.sh lab-vm:/tmp --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --quiet
@@ -86,49 +92,47 @@ sudo apt-get update
 sudo apt install python3 -y
 sudo apt-get install -y git python3-pip
 sudo apt install python3.11-venv -y
-python3 -m venv create myvenv
+python3 -m venv myvenv
 source myvenv/bin/activate
-pip3 install --upgrade pip
-pip3 install google-cloud-bigquery
-pip3 install pyarrow
-pip3 install pandas
-pip3 install db-dtypes
-pip3 install --upgrade google-cloud
+pip install --upgrade pip
+pip install google-cloud-bigquery pandas pyarrow db-dtypes google-auth
 
 export PROJECT_ID=$(gcloud config get-value project)
 export SA_EMAIL=$(curl -s "http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/email" -H "Metadata-Flavor: Google")
 
-echo "
+cat > query.py <<EOF
 from google.auth import compute_engine
 from google.cloud import bigquery
-credentials = compute_engine.Credentials(
-service_account_email='$SA_EMAIL')
+
+credentials = compute_engine.Credentials()
+
 query = '''
 SELECT name, SUM(number) as total_people
-FROM "bigquery-public-data.usa_names.usa_1910_2013"
+FROM \`bigquery-public-data.usa_names.usa_1910_2013\`
 WHERE state = 'TX'
 GROUP BY name, state
 ORDER BY total_people DESC
 LIMIT 20
 '''
+
 client = bigquery.Client(
-  project='$PROJECT_ID',
-  credentials=credentials)
+    project='$PROJECT_ID',
+    credentials=credentials
+)
+
 print(client.query(query).to_dataframe())
-" > query.py
+EOF
 
 sleep 10
 
-python3 query.py
+source myvenv/bin/activate
+python query.py
 EOF_END
 
 gcloud compute scp prepare_disk.sh bigquery-instance:/tmp --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --quiet
 
 gcloud compute ssh bigquery-instance --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --quiet --command="bash /tmp/prepare_disk.sh"
 
-
-
-#-----------------------------------------------------end----------------------------------------------------------#
 # Final message
 echo
 echo "${CYAN_TEXT}${BOLD_TEXT}=======================================================${RESET_FORMAT_FORMAT}"
