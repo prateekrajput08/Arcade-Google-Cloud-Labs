0cd28268a07c34b20b32c28478e95e7f7eebe8f9
Prateek Rajput <144830423+prateekrajput08@users.noreply.github.com>
2026-06-07 17:17:32 +0530

    Update TechCode.sh

--- Service Accounts and Roles: Fundamental/TechCode.sh
+++ Service Accounts and Roles: Fundamental/TechCode.sh
@@ -26,12 +26,12 @@ echo "${CYAN_TEXT}${BOLD_TEXT}==================================================
 echo
 
 gcloud auth list
-export PROJECT_ID=$(gcloud config get-value project)
+export DEVSHELL_PROJECT_ID=$(gcloud config get-value project)
 export ZONE=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
 export REGION=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-region])")
-gcloud iam service-accounts create my-sa-123 --display-name "subscribe to techcos"
+gcloud iam service-accounts create my-sa-123 --display-name "my service account"
 gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID --member serviceAccount:my-sa-123@$DEVSHELL_PROJECT_ID.iam.gserviceaccount.com --role roles/editor
-gcloud iam service-accounts create bigquery-qwiklab --description="subscribe to techcps" --display-name="bigquery-qwiklab"
+gcloud iam service-accounts create bigquery-qwiklab --description="subscribe to techcode" --display-name="bigquery-qwiklab"
 gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID --member="serviceAccount:bigquery-qwiklab@$DEVSHELL_PROJECT_ID.iam.gserviceaccount.com" --role="roles/bigquery.dataViewer"
 gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID --member="serviceAccount:bigquery-qwiklab@$DEVSHELL_PROJECT_ID.iam.gserviceaccount.com" --role="roles/bigquery.user"
 gcloud compute instances create bigquery-instance --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --machine-type=e2-medium --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default --metadata=enable-oslogin=true --maintenance-policy=MIGRATE --provisioning-model=STANDARD --service-account=bigquery-qwiklab@$DEVSHELL_PROJECT_ID.iam.gserviceaccount.com --scopes=https://www.googleapis.com/auth/bigquery,https://www.googleapis.com/auth/cloud-platform --create-disk=auto-delete=yes,boot=yes,device-name=bigquery-instance,image-family=debian-12,image-project=debian-cloud,mode=rw,size=10,type=projects/$DEVSHELL_PROJECT_ID/zones/$ZONE/diskTypes/pd-balanced --no-shielded-secure-boot --shielded-vtpm --shielded-integrity-monitoring --labels=goog-ec-src=vm_add-gcloud --reservation-affinity=any
